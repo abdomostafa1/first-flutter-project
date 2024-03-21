b4f129f4e156cad011e4077f8d44b195a0085723
abdo mostafa <abdomostafa19999@gmail.com>
2024-03-21 16:01:59 +0200

    i learnt circle image and rounded shapes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,18 +12,65 @@ class AppWid extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('abdo mostafa'),
-          centerTitle: true,
-          backgroundColor: const Color(0xFFd3d3d3),
-        ),
-        body: const Image(
-          image: AssetImage('images/black.jpg'),
-          filterQuality: FilterQuality.high,
-        ),
+        backgroundColor: const Color(0xFF2B455F),
+        body: businessCardApp(),
       ),
     );
   }
+}
+
+Widget businessCardApp() {
+  return Column(
+    children: [
+      CircleAvatar(
+        radius: 64,
+        backgroundColor: Colors.red,
+        child: Padding(
+          padding: const EdgeInsets.all(4), // Border radius
+          child: ClipOval(child: Image.asset('images/black.jpg')),
+        ),
+      ),
+      Text(
+        'Abdo Mostafa',
+        style: TextStyle(
+          color: Colors.white,
+          fontFamily: 'Pacifico',
+          fontSize: 32.0,
+        ),
+      ),
+      Text(
+        'FLUTTER DEVELOPER',
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 14.0,
+        ),
+      ),
+      Container(
+          margin: EdgeInsets.symmetric(horizontal: 8.0,vertical: 8.0),
+          padding: EdgeInsets.all(8.0),
+          decoration: BoxDecoration(
+              color: Colors.white, borderRadius: BorderRadius.circular(8.0)),
+          child: const Row(
+            children: [
+              Icon(Icons.call),
+              Text('(+01) 203883782'),
+            ],
+          )),
+      Container(
+        margin: EdgeInsets.symmetric(horizontal: 8.0,vertical: 8.0),
+        padding: EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+            color: Colors.white, borderRadius: BorderRadius.circular(8.0)),
+        child: Row(
+          children: [
+            Icon(Icons.email),
+            Text('ScholarTech@gmail.com'),
+          ],
+        ),
+      )
+    ],
+  );
 }
 
 Widget column() {
